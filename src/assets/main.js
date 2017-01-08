88d2b981a979795c10bb8817a3a3b97bf2bef337
Kevin Liozon <kevinliozonpro@gmail.com>
2017-01-08 00:55:10 +0000

    Display CodeSchool certifs with json call

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,40 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/kevinliozon.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      addCourses(response.courses.completed);
+    }
+  });
+
+  var $badges = $('#badges');
+
+  function addCourses(courses){
+    //add divs with the class 'course' to div with the id 'badges'
+    courses.forEach(function(course){
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      //add h3 titles to each of the previous divs generated
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      //add img to each of the previous div generated
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      //add link to each of the previous div generated
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See course'
+      }).appendTo($course);
+    });
+  }
 
 });
